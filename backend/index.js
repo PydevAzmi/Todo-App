2be92ebc8b72be4614ddaa0b6a23dc5c2a32a1be
Mohamed Azmi <pydevazmi@gmail.com>
2024-12-04 11:06:50 +0200

    Allow CORS Headers to allow all orgins

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const dotenv = require('dotenv');
 const express = require('express');
+var cors = require('cors')
 const taskRoutes = require('./src/routes/taskRoutes');
 
 require('./src/config/dbConnect').connect();
@@ -10,9 +11,15 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 // routes
-app.use('/tasks', taskRoutes);
+app.use('/tasks',  cors(corsOptions), taskRoutes);
 
 // Server
 const server = http.createServer(app);
